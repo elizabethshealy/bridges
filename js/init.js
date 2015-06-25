f90c70b0a952e5f556063be4ea1f57e4b6a75262
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2015-06-25 10:07:55 -0400

    Parsing data, updating files, showing bridges html, templating

--- js/init.js
+++ js/init.js
@@ -1,11 +1,36 @@
 'use strict';
 
 var $ = require('jquery');
+var _ = require('underscore');
+var views = require('views');
 
 $.ajax({
   url: 'data/bridges.data',
   method: 'GET'
 })
-  .then(function (data) {
-    console.log (data);
-});
+.then(parseBridgesCsv)
+.then(renderBridges);
+
+function parseBridgesCsv(bridgesCsv) {
+  return bridgesCsv
+    .split('\n')
+    .map(function (record) {
+      var cells = record.split(',');
+
+      return {
+        id: cells[0],
+        erected: cells[3],
+        lanes: cells[6],
+        material: cells[9],
+        type: cells[12]
+      };
+    });
+}
+
+function renderBridges(bridgesArray) {
+  var bridgesTemplate = views['bridges-template'];
+  var templateFn = _.template(bridgesTemplate, { variable: 'm' });
+  var bridgesHTML = templateFn({ bridges: bridgesArray });
+
+  $('.main-content').html(bridgesHTML);
+}
